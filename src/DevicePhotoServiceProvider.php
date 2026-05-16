6a5bfdaed0ae1703ed847131bceeceebd54caa42
Wizball <WizballESY@users.noreply.github.com>
2026-05-16 13:06:22 +0200

    Register package config views and hooks

--- src/DevicePhotoServiceProvider.php
+++ src/DevicePhotoServiceProvider.php
@@ -3,16 +3,34 @@
 namespace WizballEsy\LibreNmsDevicePhoto;
 
 use Illuminate\Support\ServiceProvider;
+use LibreNMS\Interfaces\Plugins\PluginManagerInterface;
+use App\Plugins\Hooks\DeviceOverviewHook;
+use App\Plugins\Hooks\MenuEntryHook;
+use App\Plugins\Hooks\PageHook;
+use App\Plugins\Hooks\SettingsHook;
+use WizballEsy\LibreNmsDevicePhoto\Hooks\DeviceOverview;
+use WizballEsy\LibreNmsDevicePhoto\Hooks\Menu;
+use WizballEsy\LibreNmsDevicePhoto\Hooks\Page;
+use WizballEsy\LibreNmsDevicePhoto\Hooks\Settings;
 
 class DevicePhotoServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../config/device-photo.php', 'device-photo');
     }
 
     public function boot(): void
     {
-        //
+        $pluginName = 'device-photo';
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'device-photo');
+
+        $pluginManager = $this->app->make(PluginManagerInterface::class);
+
+        $pluginManager->publishHook($pluginName, DeviceOverviewHook::class, DeviceOverview::class);
+        $pluginManager->publishHook($pluginName, MenuEntryHook::class, Menu::class);
+        $pluginManager->publishHook($pluginName, PageHook::class, Page::class);
+        $pluginManager->publishHook($pluginName, SettingsHook::class, Settings::class);
     }
 }
