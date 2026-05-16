f628c61034e707fe095b88e8cd688f461aabe277
Wizball <WizballESY@users.noreply.github.com>
2026-05-16 14:44:40 +0200

    Add package action route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use WizballEsy\LibreNmsDevicePhoto\Http\Controllers\ActionController;
 use WizballEsy\LibreNmsDevicePhoto\Http\Controllers\PhotoController;
 
 Route::middleware(['web'])
     ->get('plugin/device-photo-package/image', [PhotoController::class, 'show'])
     ->name('device-photo.image');
+
+Route::middleware(['web'])
+    ->post('plugin/device-photo-package/action', [ActionController::class, 'handle'])
+    ->name('device-photo.action');
