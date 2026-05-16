c09d61fc28bbb3618059910641e81105c59cda6c
Wizball <WizballESY@users.noreply.github.com>
2026-05-16 12:54:09 +0200

    Add initial package service provider

---
+++ src/DevicePhotoServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WizballEsy\LibreNmsDevicePhoto;
+
+use Illuminate\Support\ServiceProvider;
+
+class DevicePhotoServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
