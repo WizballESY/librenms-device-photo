3d61fa44f28ffa65919811bb5839d00f36446898
Wizball <WizballESY@users.noreply.github.com>
2026-05-23 15:21:04 +0200

    Validate device exists before adding photo links

--- src/Http/Controllers/ActionController.php
+++ src/Http/Controllers/ActionController.php
@@ -1431,7 +1431,7 @@ class ActionController extends Controller
 
     private function addLink(Request $request, int $deviceId)
     {
-        if ($deviceId < 1) {
+        if (! $this->findExistingDevice($deviceId)) {
             if ($this->wantsJsonResponse($request)) {
                 return $this->jsonStatus('device_not_found', false, 404);
             }
@@ -1509,7 +1509,7 @@ class ActionController extends Controller
 
     private function addIncomingLink(Request $request, int $deviceId)
     {
-        if ($deviceId < 1) {
+        if (! $this->findExistingDevice($deviceId)) {
             if ($this->wantsJsonResponse($request)) {
                 return $this->jsonStatus('device_not_found', false, 404);
             }
