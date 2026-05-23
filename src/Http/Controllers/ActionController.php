b027557eff002b74c3f56c2ca056507953a627e7
Wizball <WizballESY@users.noreply.github.com>
2026-05-23 15:22:57 +0200

    Validate device exists before removing outgoing links

--- src/Http/Controllers/ActionController.php
+++ src/Http/Controllers/ActionController.php
@@ -218,7 +218,7 @@ class ActionController extends Controller
 
     private function removeOutgoingLink(Request $request, int $deviceId)
     {
-        if ($deviceId < 1) {
+        if (! $this->findExistingDevice($deviceId)) {
             if ($this->wantsJsonResponse($request)) {
                 return $this->jsonStatus('device_not_found', false, 404);
             }
